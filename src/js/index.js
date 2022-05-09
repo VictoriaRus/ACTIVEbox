f53c5147ca2429dff95ae89eb7b9d72d4b591f3f
Вика <ruseckaya@cmit.ru>
2022-05-09 17:54:16 +0300

    add: sections

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,49 @@
-console.log("alo");
+import "./slick";
+const jquery = require("jquery")
+window.$ = window.jQuery = jquery;
+
+$('.slider').slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 1000,
+    easing: 'ease',
+    infinite: true,
+    autoplay: false,
+    draggable: false,
+    swipe: true,
+    touchThreshold: 10,
+    waitForAnimate: false
+});
+
+$('.burger').click(function () {
+
+    if ($(".burger i").hasClass("fa-solid fa-bars")) {
+        $('.burger i').removeClass('fa-solid fa-bars');
+        $('.burger i').addClass('fa-solid fa-square-xmark');
+    } else {
+        $('.burger i').removeClass('fa-solid fa-square-xmark');
+        $('.burger i').addClass('fa-solid fa-bars');
+    }
+    $('.wrapper').toggleClass('wrapper-open');
+    $('.header__nav').toggleClass('open');
+    $('.header__navigation').toggleClass('open');
+    $('body').toggleClass('fixed-page');
+});
+
+$('.header__navigation-item').click(function () {
+    if ($(".burger i").hasClass("fa-solid fa-bars")) {
+        $('.burger i').removeClass('fa-solid fa-bars');
+        $('.burger i').addClass('fa-solid fa-square-xmark');
+    } else {
+        $('.burger i').removeClass('fa-solid fa-square-xmark');
+        $('.burger i').addClass('fa-solid fa-bars');
+    }
+    $('.wrapper').toggleClass('wrapper-open');
+    $('.header__nav').toggleClass('open');
+    $('.header__navigation').toggleClass('open');
+    $('body').toggleClass('fixed-page');
+});
+
+
